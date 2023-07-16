8a639e226894fa4717801ee80e67fa8ac803c9d2
Ashik465 <helloashikul@gmail.com>
2023-07-16 18:52:23 +0600

    count duplicates in the array done

--- count-duplicates.js
+++ count-duplicates.js
@@ -1,1 +1,17 @@
-const fruits = [apple, orange, mango,orange, mango, jackfruit]
+const fruits = ["apple", "orange", "mango","orange", "mango", "jackfruit"]
+
+const countDuplicate = (fruit) => {
+const newObject ={}
+
+ fruit.map(d=>{
+    newObject[d] = (newObject[d] || 0)+1 ;
+ }
+
+ )
+
+return newObject ;
+
+}
+
+
+console.log(countDuplicate(fruits))
